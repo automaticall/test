3a118574a17e45c730c79d2f6103164d1aee5f9b
EDJINEDJA <edjinedjalaris@gmail.com>
2023-07-11 14:23:20 +0200

    commit on draft local repository#(0007)

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+import unittest
+
+import sys
+sys.path.append("..")
+
+from src.outils.outils import Outils
+
+class Test(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.parser = Outils() 
+        
+    def checkIfPrimaryInstanceIsWellDefine(self):
+        self.assertIsInstance(self.parser , Outils)
+
+    
+if __name__=="__maine__":
+    unittest.main()
